7985849f7f8ad4f561850e72bb4c3a5579c1af74
mswift42 <martin.haesler@gmail.com>
2016-11-05 13:24:36 +0000

    Add mock Recipes

---
+++ lib/services/mockRecipeService/MockRecipeService.dart
@@ -0,0 +1,41 @@
+import 'package:eggflutter/services/Recipe.dart';
+import 'dart:convert';
+
+final List<Recipe> mockrecipes = [
+  new Recipe(
+      "The Pioneer Woman",
+      "French Onion Soup Stuffed Mushrooms",
+      "http://thepioneerwoman.com/cooking/2009/11/french-onion-soup-stuffed-mushrooms/",
+      "http://static.food2fork.com/5200405868_e86da8e6e8_oceea.jpg",
+      "http://thepioneerwoman.com"),
+  new Recipe(
+      "All Recipes",
+      "Easy Slow Cooker French Dip",
+      "http://allrecipes.com/Recipe/Easy-Slow-Cooker-French-Dip/Detail.aspx",
+      "http://static.food2fork.com/103167cea.jpg",
+      "http://allrecipes.com"),
+  new Recipe(
+      "Closet Cooking",
+      "Pumpkin Pie French Toast",
+      "http://www.closetcooking.com/2009/11/pumpkin-pie-french-toast.html",
+      "http://static.food2fork.com/PumpkinPieFrenchToast15002845c7fe.jpg",
+      "http://closetcooking.com"),
+  new Recipe(
+      "The Pioneer Woman",
+      "Perfect French Fries",
+      "http://thepioneerwoman.com/cooking/2012/01/perfect-french-fries/",
+      "http://static.food2fork.com/fries4d41.jpg",
+      "http://thepioneerwoman.com"),
+  new Recipe(
+      "The Pioneer Woman",
+      "Baked French Toast",
+      "http://thepioneerwoman.com/cooking/2010/05/cinnamon-baked-french-toast/",
+      "http://static.food2fork.com/4573173616_0ed83c1ee1_o91e9.jpg",
+      "http://thepioneerwoman.com"),
+  new Recipe(
+      "Two Peas and Their Pod",
+      "Cannoli Stuffed French Toast",
+      "http://www.twopeasandtheirpod.com/cannoli-stuffed-french-toast/",
+      "http://static.food2fork.com/cannolistuffedfrenchtoast48a37.jpg",
+      "http://www.twopeasandtheirpod.com")
+];

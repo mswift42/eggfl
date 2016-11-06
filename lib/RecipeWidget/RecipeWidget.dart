4d7420e9837814894c889feb8fda21631ae09b34
mswift42 <martin.haesler@gmail.com>
2016-11-06 20:16:45 +0000

    Add image, set bg color

--- lib/RecipeWidget/RecipeWidget.dart
+++ lib/RecipeWidget/RecipeWidget.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:eggflutter/services/mockRecipeService/MockRecipeService.dart';
+
+class RecipeWidget extends StatelessWidget {
+  @override
+  build(BuildContext context) {
+    return new Card(
+        color: Colors.blueGrey[100],
+        child: new Padding(
+          padding: const EdgeInsets.fromLTRB(2.0, 8.0, 2.0, 3.0),
+          child: new Column(
+            children: <Widget>[
+              new Text(mockrecipes[0].title),
+              new Image(image: new NetworkImage(mockrecipes[0].image_url)),
+            ],
+          ),
+        ));
+  }
+}

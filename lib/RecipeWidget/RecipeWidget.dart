a32961ba1ef33b1bacf6252a4833064844398cb3
mswift42 <martin.haesler@gmail.com>
2016-11-07 18:32:03 +0000

    Add LinkTextSpan class.

--- lib/RecipeWidget/RecipeWidget.dart
+++ lib/RecipeWidget/RecipeWidget.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:eggflutter/services/mockRecipeService/MockRecipeService.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/services.dart' show UrlLauncher;
+
+class LinkTextSpan extends TextSpan {
+  LinkTextSpan({TextStyle style, String url, String text})
+      : super(
+            style: style,
+            text: text ?? url,
+            recognizer: new TapGestureRecognizer()
+              ..onTap = () {
+                UrlLauncher.launch(url);
+              });
+}
 
 class RecipeWidget extends StatelessWidget {
   @override
   build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+    final TextStyle linkStyle = themeData.textTheme.body2.copyWith(color: themeData.accentColor);
     return new Card(
         color: Colors.white,
         child: new Padding(
@@ -14,7 +29,12 @@ class RecipeWidget extends StatelessWidget {
               new Image(image: new NetworkImage(mockrecipes[0].image_url)),
               new Row(children: <Widget>[
                 new Flexible(child: new Text(mockrecipes[0].publisher)),
-                new Text(mockrecipes[0].publisher_url)
+                new RichText(text : new TextSpan(
+                    children: <TextSpan>[
+                    new LinkTextSpan(style: linkStyle, url: mockrecipes[0].publisher_url,
+                        text: mockrecipes[0].publisher),
+                    ]
+                ))
               ])
             ],
           ),

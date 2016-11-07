8f27a214214e8b2a2551e90414f339fab08de993
mswift42 <martin.haesler@gmail.com>
2016-11-07 16:59:02 +0000

    Add publisher / publisherurl

--- lib/RecipeWidget/RecipeWidget.dart
+++ lib/RecipeWidget/RecipeWidget.dart
@@ -5,13 +5,17 @@ class RecipeWidget extends StatelessWidget {
   @override
   build(BuildContext context) {
     return new Card(
-        color: Colors.blueGrey[100],
+        color: Colors.white,
         child: new Padding(
           padding: const EdgeInsets.fromLTRB(2.0, 8.0, 2.0, 3.0),
           child: new Column(
             children: <Widget>[
               new Text(mockrecipes[0].title),
               new Image(image: new NetworkImage(mockrecipes[0].image_url)),
+              new Row(children: <Widget>[
+                new Flexible(child: new Text(mockrecipes[0].publisher)),
+                new Text(mockrecipes[0].publisher_url)
+              ])
             ],
           ),
         ));

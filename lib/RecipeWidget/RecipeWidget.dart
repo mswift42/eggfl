e9d09ef2725d7f8d8a205e61dbb0b7d9d9adb874
mswift42 <martin.haesler@gmail.com>
2016-11-08 18:22:29 +0000

    Apply padding to title and bottom row

--- lib/RecipeWidget/RecipeWidget.dart
+++ lib/RecipeWidget/RecipeWidget.dart
@@ -18,26 +18,28 @@ class RecipeWidget extends StatelessWidget {
   @override
   build(BuildContext context) {
     final ThemeData themeData = Theme.of(context);
-    final TextStyle linkStyle = themeData.textTheme.body2.copyWith(color: themeData.accentColor);
+    final TextStyle linkStyle =
+        themeData.textTheme.body2.copyWith(color: themeData.accentColor);
     return new Card(
         color: Colors.white,
-        child: new Padding(
-          padding: const EdgeInsets.fromLTRB(2.0, 8.0, 2.0, 3.0),
-          child: new Column(
+         child: new Column(
             children: <Widget>[
-              new Text(mockrecipes[0].title),
+              new Padding(padding: const EdgeInsets.all(16.0) ,child:
+              new Text(mockrecipes[0].title)),
               new Image(image: new NetworkImage(mockrecipes[0].image_url)),
+              new Padding(padding: const EdgeInsets.all(16.0), child:
               new Row(children: <Widget>[
                 new Flexible(child: new Text(mockrecipes[0].publisher)),
-                new RichText(text : new TextSpan(
-                    children: <TextSpan>[
-                    new LinkTextSpan(style: linkStyle, url: mockrecipes[0].publisher_url,
-                        text: mockrecipes[0].publisher),
-                    ]
-                ))
-              ])
+                new RichText(
+                    text: new TextSpan(children: <TextSpan>[
+                  new LinkTextSpan(
+                      style: linkStyle,
+                      url: mockrecipes[0].publisher_url,
+                      text: mockrecipes[0].publisher),
+                ]))
+              ])),
             ],
           ),
-        ));
+        );
   }
 }

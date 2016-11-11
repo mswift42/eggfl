0a7063976c3b1c91b11066f3c50751c429786f43
mswift42 <martin.haesler@gmail.com>
2016-11-11 17:19:35 +0000

    Add field scrollableKey

--- lib/RecipeWidget/RecipeListWidget.dart
+++ lib/RecipeWidget/RecipeListWidget.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:eggflutter/RecipeWidget/RecipeWidget.dart';
-import 'package:eggflutter/services/mockRecipeService/MockRecipeService.dart' show mockrecipes;
-import 'package:eggflutter/services/Recipe.dart'  show Recipe;
+import 'package:eggflutter/services/mockRecipeService/MockRecipeService.dart'
+    show mockrecipes;
+import 'package:eggflutter/services/Recipe.dart' show Recipe;
 
-class RecipeBlockItem extends StatelessWidget {
-  List<Widget> _recipewidgets = mockrecipes.map((i) => new RecipeWidget(i.title, i.image_url,
-      i.publisher, i.publisher_url)).toList();
+class RecipeBlockState extends State<RecipeBlockItem> {
+  static final GlobalKey<ScrollableState> _scrollableKey =
+      new GlobalKey<ScrollableState>();
+  List<Widget> recipeitems = mockrecipes
+      .map((i) =>
+          new RecipeWidget(i.title, i.image_url, i.publisher, i.publisher_url))
+      .toList();
+
   @override
   Widget build(BuildContext context) {
     return new Block(
-        children: _recipewidgets,
-    padding: const EdgeInsets.all(2.0),
-    initialScrollOffset: 1.0,
-    scrollableKey: key,
+      children: recipeitems,
+      padding: const EdgeInsets.all(2.0),
+      initialScrollOffset: 1.0,
     );
   }
+}
 
+class RecipeBlockItem extends StatefulWidget {
+  RecipeBlockItem({Key key}) : super(key: key);
+  @override
+  RecipeBlockState createState() => new RecipeBlockState();
 }

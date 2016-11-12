82819d94e60beb7ef7c9d2960607fdab5b94becb
mswift42 <martin.haesler@gmail.com>
2016-11-12 10:49:21 +0000

    Add RecipeListPage.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:eggflutter/services/mockRecipeService/MockRecipeService.dart';
+import 'RecipeWidget/RecipeWidget.dart';
+import 'package:eggflutter/services/Recipe.dart';
 
 class EggCrackinHome extends StatelessWidget {
-  static final GlobalKey<ScrollableState> scrollablekey =
+  final GlobalKey<ScrollableState> scrollablekey =
       new GlobalKey<ScrollableState>();
 
   @override
   Widget build(BuildContext context) {
-    return new RecipeListPage(recipes: mockrecipes, scrollablekey: scrollablekey);
+    return new MaterialApp(title: 'EggCrackin', home: new RecipeListPage());
   }
 }
 
-void main() {}
+class RecipeListPage extends StatelessWidget {
+  final GlobalKey<ScrollableState> scrollablekey =
+      new GlobalKey<ScrollableState>();
+  List<Widget> _recipes = mockrecipes.map((i) =>
+      new RecipeWidget(i.title, i.image_url, i.publisher, i.publisher_url)).toList();
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      scrollableKey: scrollablekey,
+      appBar: new AppBar(title: new Text("Egg Crackin!")),
+      body: new Block(
+        scrollableKey: scrollablekey,
+        children: _recipes,
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(new EggCrackinHome());
+}

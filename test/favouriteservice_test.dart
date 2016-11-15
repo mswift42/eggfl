b85049b4f68f1eff5e76d15acfe6184ca3fd56eb
mswift42 <martin.haesler@gmail.com>
2016-11-15 21:24:49 +0000

    Add Favourite Service tests

--- test/favouriteservice_test.dart
+++ test/favouriteservice_test.dart
@@ -17,4 +17,22 @@ void main() {
     expect(fs.favourites.length, 1);
     expect(fs.isFavourite(r1), true);
   });
+  test('removeFavourite removes the recipe from favourites.', () {
+    var fs = new FavouriteService();
+    Recipe r1 = mockrecipes[0];
+    fs.addFavourite(r1);
+    expect(fs.isFavourite(r1), true);
+    fs.deleteFavourite(r1);
+    expect(fs.isFavourite(r1), false);
+  });
+  test('isFavourite checks if a given recipe is in favourites', () {
+      var fs = new FavouriteService();
+    Recipe r1 = mockrecipes[0];
+    expect(fs.isFavourite(r1), false);
+    fs.addFavourite(r1);
+    expect(fs.isFavourite(r1), true);
+    expect(fs.isFavourite(mockrecipes[1]), false);
+    fs.addFavourite(mockrecipes[1]);
+    expect(fs.isFavourite(mockrecipes[1]), true);
+  });
 }

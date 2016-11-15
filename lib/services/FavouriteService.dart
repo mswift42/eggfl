952b6a00656ea04e93ca2c1e00cfd5db881ada9e
mswift42 <martin.haesler@gmail.com>
2016-11-15 20:43:40 +0000

    Add add / delete / isFavourite methods.

--- lib/services/FavouriteService.dart
+++ lib/services/FavouriteService.dart
@@ -1,1 +1,19 @@
 import 'package:eggflutter/services/Recipe.dart' show Recipe;
+
+class FavouriteService {
+  final Set<Recipe> _favourites = new Set<Recipe>();
+
+  void addFavourite(Recipe recipe) {
+    _favourites.add(recipe);
+  }
+
+  void deleteFavourite(Recipe recipe) {
+    _favourites.remove(recipe);
+  }
+
+  bool isFavourite(Recipe recipe) {
+    return _favourites.contains(recipe);
+  }
+
+
+}

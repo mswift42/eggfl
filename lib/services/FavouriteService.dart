e52a655c6ac194fb0a95dfdc0e6694d50be2f444
mswift42 <martin.haesler@gmail.com>
2016-11-15 20:44:38 +0000

    Add Favourite getter.

--- lib/services/FavouriteService.dart
+++ lib/services/FavouriteService.dart
@@ -3,6 +3,8 @@ import 'package:eggflutter/services/Recipe.dart' show Recipe;
 class FavouriteService {
   final Set<Recipe> _favourites = new Set<Recipe>();
 
+  Set<Recipe> get favourites => _favourites;
+
   void addFavourite(Recipe recipe) {
     _favourites.add(recipe);
   }

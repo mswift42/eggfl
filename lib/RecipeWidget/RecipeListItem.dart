5b61ab2f9158fd2434308117ec5cd484bb872978
mswift42 <martin.haesler@gmail.com>
2016-11-22 14:24:29 +0000

    Add RecipeListItem properties.

--- lib/RecipeWidget/RecipeListItem.dart
+++ lib/RecipeWidget/RecipeListItem.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/gestures.dart' show TapGestureRecognizer;
 import 'package:flutter/services.dart' show UrlLauncher;
 
 class LinkTextSpan extends TextSpan {
   LinkTextSpan({TextStyle style, String url, String text})
       : super(
-      style: style,
-      text: text ?? url,
-      recognizer: new TapGestureRecognizer()
-        ..onTap = () {
-          UrlLauncher.launch(url);
-        });
+            style: style,
+            text: text ?? url,
+            recognizer: new TapGestureRecognizer()
+              ..onTap = () {
+                UrlLauncher.launch(url);
+              });
+}
+
+class RecipeListItem extends StatelessWidget {
+  String title;
+  String image_url;
+  String publisher;
+  String recipe_id;
+  RecipeListItem(this.title, this.image_url, this.publisher, this.recipe_id);
+
+  @override
+  build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+  }
 }
